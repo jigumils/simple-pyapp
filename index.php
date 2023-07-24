83696dd948fce10f6113809f557741576e72529b
Aji Gumilang <aji.gumilang2022@gmail.com>
2023-07-24 15:17:09 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+$conn = mysqli_connect("testsql","root","mantap12345","mantap");
+
+if ($conn->connect_error){
+	die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected to mySQL Successfully!";
+
+$sql = "select name from account where id = '1'";
+
+//query to  database
+$result = $conn->query($sql);
+
+//print to web
+$row = $result->fetch_assoc();
+	echo "<br>Hello, " . $row["name"] . ", Have a nice day!" ;
+
+?>
